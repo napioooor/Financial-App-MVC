133f6498d28a0d1b76663309f3753f45048707d4
Jan <jan.napiorkowski@o2.pl>
2022-08-31 20:12:05 +0200

    Minor changes

--- App/Config.php
+++ App/Config.php
@@ -5,14 +5,14 @@ class Config {
     const DB_HOST = 'budget.jan-napiorkowski.profesjonalnyprogramista.pl.mysql.dhosting.pl';
     const DB_NAME = 'aew9ah_budgetja';
     const DB_USER = 'me7ati_budgetja';
-    const DB_PASSWORD = 'Eiv4Ahngoa8A';
+    const DB_PASSWORD = '***';
     const SHOW_ERRORS = true;
-    const SECRET_KEY = '4sGz4oAsla3ORnzMzbckwWGhWyjvuNkn';
+    const SECRET_KEY = '***';
 
     //Email config
     const MAIL_HOST = 'poczta.o2.pl';
     const MAIL_USERNAME = 'jan.napiorkowski@o2.pl'; 
-    const MAIL_PASSWORD = 'jasiek1997';
+    const MAIL_PASSWORD = '***';
     const MAIL_SECURITY = 'ssl';
     const MAIL_PORT = 465;
 }
